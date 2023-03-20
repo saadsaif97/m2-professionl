42f3582ccf7ea92d9ec65a1eae49d45973f27648
saadgfx97 <saadgfx97@gmail.com>
2023-03-20 12:36:05 +0500

    added api token and api url in admin system and updated their config values and updated the DI accordingly

--- app/code/SaadSaif/OrderExport/Model/Config.php
+++ app/code/SaadSaif/OrderExport/Model/Config.php
@@ -8,6 +8,8 @@ use Magento\Store\Model\ScopeInterface;
 class Config
 {
     const CONFIG_PATH_ENABLED = "sales/order_export/enabled";
+    const CONFIG_PATH_API_URL = "sales/order_export/api_url";
+    const CONFIG_PATH_API_TOKEN = "sales/order_export/api_token";
 
     private ScopeConfigInterface $scopeConfig;
 
@@ -29,5 +31,17 @@ class Config
     public function isEnabled(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): bool
     {
         return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_ENABLED, $scopeType, $scopeCode);
+    }
+
+    public function getApiUrl(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): string
+    {
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_API_URL, $scopeType, $scopeCode);
+        return ($value != null) ? (string) $value : '';
+    }
+
+    public function getApiToken(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): string
+    {
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_API_TOKEN, $scopeType, $scopeCode);
+        return ($value != null) ? (string) $value : '';
     }
 }
